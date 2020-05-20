543862b9e7cf3d80430a07a6fcdc617bb6c914da
Leslie <lesliec38@hotmail.fr>
2020-05-20 15:23:53 +0200

    little css

--- src/components/contents/my-games/NewGameCard.jsx
+++ src/components/contents/my-games/NewGameCard.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './MyGameCard';
+import Swal from 'sweetalert2';
 import DisplayRating from './DisplayRating';
 import InfoCard from './InfoCard';
 
